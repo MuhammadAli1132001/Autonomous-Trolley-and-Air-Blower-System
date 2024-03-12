0ebebb379d9db957152496aeaf7ed242d99a5797
Muhammad Ali <engr.alikhan2001@gmail.com>
2024-03-12 12:17:08 +0500

    setting trolley initial condition with enabled button and direction of both motors

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
-#include <Arduino.h>
-#define led 1
+/* Air Blower System 
+Design an industrial usage Mover trolley and Air bolwer system, implement two motors for trolley moving and for Air blower/exhaust.
+The trolley autonomously move from and to two parking positions which is stoped by the limit switches at each end of the parking.
+I have used BTS7960 for motors controlling in term of forward/reverse direction, while
+pulse width modulation(PWM) is programmatically configured for variable motor speeds. A reset push buton ensures restarting of the whole process.*/
 
-// put function declarations here:
+#include <Arduino.h>
+
+#define Push_Enable 11
 #define Limit_switchA  2
 #define Limit_switchB 3
 
@@ -19,17 +24,20 @@ bool initialStart = false;
 uint8_t state = 0;
 uint8_t i = 0;
 
+void Started(void);
 void Move_forward(void);
 void Move_reverse(void);
 void Stop();
-void M1_control();
-void M2_control();
+void SwitchA_control();
+void SwitchB_control();
 
 void setup()
 {
 
+  pinMode(Push_Enable, INPUT);
   pinMode(Limit_switchA, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(Limit_switchB, INPUT);
+
   Serial.begin(115200);
 
   pinMode(M1_R_Pwm, OUTPUT);
@@ -47,8 +55,9 @@ void setup()
   digitalWrite(R_IS, LOW);
   digitalWrite(L_IS, LOW);
 
-  attachInterrupt(digitalPinToInterrupt(Limit_switchA), M1_control, RISING);
-  attachInterrupt(digitalPinToInterrupt(Limit_switchB), M2_control, RISING);
+  // attachInterrupt(digitalPinToInterrupt(Push_Enable), Started, RISING);
+  attachInterrupt(digitalPinToInterrupt(Limit_switchA), SwitchA_control, RISING);
+  attachInterrupt(digitalPinToInterrupt(Limit_switchB), SwitchB_control, RISING);
 
   delay(100);
 }
@@ -71,34 +80,44 @@ void loop()
     break;
   }
 
-
-  Serial.println(state);
+  Started();
+  Serial.print("state ");Serial.println(state);
   delay(10);
 }
 
-void M1_control()
+void Started()
+{
+  if (!digitalRead(Push_Enable))
+  {
+    Serial.println("Enabled is Pressed");
+    state = 1;  
+    digitalWrite(Push_Enable, HIGH);
+    delay(20);
+
+  }
+
+
+}
+void SwitchA_control()                                   //ISR for limit A pressed
 {
     
     Serial.println("limit switch A Pressed");
-    digitalWrite(led, analogRead(~led));
 
     analogWrite(M1_R_Pwm, 0);
     analogWrite(M1_L_Pwm, 0);
     analogWrite(M2_R_Pwm, 0);
     analogWrite(M2_L_Pwm, 0);
-    delay(2000);
     initialStart = false;
-    state = 2;
+    state = 0;
     
 }
 
-
-void M2_control()
+ 
+void SwitchB_control()                                     //ISR for limit B pressing
 {
     int delays = 0;
 
     Serial.println("limit switch B Pressed");
-    digitalWrite(led, analogRead(~led));
 
     analogWrite(M1_R_Pwm, 0);
     analogWrite(M1_L_Pwm, 0);
@@ -107,8 +126,9 @@ void M2_control()
     delay(1000);
 
     Serial.println("exhaust fan ruining");
+    delay(300);
 
-    while (delays != 10000)
+    while (delays <= 5000)                           //moving M2 with full speed opposite direction for Exhaust, upto 10 seconds
     {    
     analogWrite(M2_L_Pwm, 0);
     analogWrite(M2_R_Pwm, 255);
@@ -117,14 +137,14 @@ void M2_control()
     Serial.println(delays);
 
     }
-
+    delay(500);
     initialStart = false;
-    state = 0;
+    state = 2;
  
     
 }
-void Stop()
-{
+void Stop()                                             //default and after reverse direction state
+{   
 
   Serial.println("the trolley is currently stoped");
   analogWrite(M1_L_Pwm, 0);
@@ -132,20 +152,44 @@ void Stop()
   analogWrite(M2_L_Pwm, 0);
   analogWrite(M2_R_Pwm, 0);
 
-  delay(10000);
-  state = 1;
+  delay(500);
+  // state = 1;
 
 }
 
-void Move_forward()
+void Move_forward()                              //moving the trolley forward with air blower as well
 {
   if (!initialStart)
   {
-    for (int i = 0; i <= 255; i++)
+    for (int i = 0; i <= 150; i++)
     {      
-      analogWrite(M2_R_Pwm, i); 
-      analogWrite(M1_L_Pwm, 0); 
+      analogWrite(M1_R_Pwm, 0);
+      analogWrite(M1_L_Pwm, i);
+      analogWrite(M2_R_Pwm, 0); 
+      analogWrite(M2_L_Pwm, i);
+      delay(20);
+    }
+    initialStart = true;
+  }
+
+  else
+  {
+    analogWrite(M2_L_Pwm, 125);
+    analogWrite(M1_L_Pwm, 125);
+
+  }
+}
+
+void Move_reverse()                            //moving the trolley in reverse direction with air blower
+{
+  if (!initialStart)
+  
+  {
+    for (int i = 0; i <= 125; i++)
+    {      
       analogWrite(M1_R_Pwm, i);
+      analogWrite(M1_L_Pwm, 0);
+      analogWrite(M2_R_Pwm, i);
       analogWrite(M2_L_Pwm, 0);
       delay(20);
     }
@@ -154,50 +198,11 @@ void Move_forward()
 
   else
   {
-    analogWrite(M2_R_Pwm, 150);
-    analogWrite(M1_R_Pwm, 150);
+    analogWrite(M1_R_Pwm, 125);
+    analogWrite(M2_R_Pwm, 125);
 
   }
 }
-
-void Move_reverse()
-{
-  if (!initialStart)
-  {
-    for (int i = 0; i <= 255; i++)
-    {      
-      analogWrite(M2_L_Pwm, i);
-      analogWrite(M1_R_Pwm, 0);
-      analogWrite(M1_L_Pwm, i);
-      analogWrite(M2_R_Pwm, 0);
-      delay(30);
-    }
-    initialStart = true;
-  }
-
-  else
-  {
-    analogWrite(M1_L_Pwm, 150);
-    analogWrite(M2_L_Pwm, 150);
-
-    // if (digitalRead(Limit_switchB))
-    // {
-    //   Serial.println("limit switch B Pressed");
-    //   digitalWrite(led, analogRead(~led));
-
-    //   analogWrite(M1_R_Pwm, 0);
-    //   analogWrite(M1_R_Pwm, 0);
-    //   delay(1000);
-    //   initialStart = false;
-    //   state = 0;
- 
-    // }
-  }
-}
-
-
-
-
 
 // while (digitalRead(switch_button))
 // {
